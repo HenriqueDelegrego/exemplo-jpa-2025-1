14341c91dcb46ca1e82c95f1e9c8b842ce566e68
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-23 20:04:38 -0300

    adiciona anotações jpa

--- src/main/java/com/delegrego/exemplo_jpa_2025_1/entity/DepartamentoEntity.java
+++ src/main/java/com/delegrego/exemplo_jpa_2025_1/entity/DepartamentoEntity.java
@@ -1,9 +1,23 @@
 package com.delegrego.exemplo_jpa_2025_1.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "departamento")
 public class DepartamentoEntity {
 	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_departamento")
 	private int idDepartamento;
 	
+	@Column(name = "nm_departamento")
 	private String nmDepartamento;
 
 	public DepartamentoEntity(int idDepartamento, String nmDepartamento) {

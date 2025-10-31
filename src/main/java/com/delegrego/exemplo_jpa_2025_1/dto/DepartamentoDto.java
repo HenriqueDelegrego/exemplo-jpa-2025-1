cea4bf3423ae14f8658f24e4a1f3683376484766
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-31 13:25:16 -0300

    adiciona bean validation

--- src/main/java/com/delegrego/exemplo_jpa_2025_1/dto/DepartamentoDto.java
+++ src/main/java/com/delegrego/exemplo_jpa_2025_1/dto/DepartamentoDto.java
@@ -1,9 +1,14 @@
 package com.delegrego.exemplo_jpa_2025_1.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class DepartamentoDto {
 
 	private int idDepartamento;
 
+	@NotBlank
+	@Size(max = 50)
 	private String nmDepartamento;
 
 	public DepartamentoDto(int idDepartamento, String nmDepartamento) {

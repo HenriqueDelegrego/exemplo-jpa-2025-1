3c1a525187ddd5b949831b428e145886485bd5ed
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-11-04 18:47:40 -0300

    Adiciona bean validation

--- src/main/java/com/delegrego/exemplo_jpa_2025_1/dto/FuncionarioDto.java
+++ src/main/java/com/delegrego/exemplo_jpa_2025_1/dto/FuncionarioDto.java
@@ -1,17 +1,31 @@
 package com.delegrego.exemplo_jpa_2025_1.dto;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
 public class FuncionarioDto {
 
 	private int idFuncionario;
 
+	@NotBlank(message = "Nome do funcionário não pode ser vazio")
+	@Size(max = 100, message = "Nome do funcionário não pode ultrapassar 100 caracteres")
 	private String nome;
 
+	@NotBlank(message = "Email do funcionário não pode ser vazio")
+	@Size(max = 50, message = "Email do funcionário não pode ultrapassar 50 caracteres")
+	@Email(message = "Email do funcionário inválido")
 	private String email;
 
+	@NotBlank(message = "Senha do funcionário não pode ser vazia")
+	@Size(max = 50, message = "Senha do funcionário não pode ultrapassar 50 caracteres")
 	private String senha;
 
+	@Digits(integer = 10, fraction = 2, message = "Salário do funcionário inválido")
+	@PositiveOrZero(message = "Salário do funcionário não pode ser negativo")
 	private double salario;
-
 	private int idDepartamento;
 
 	public FuncionarioDto() {
